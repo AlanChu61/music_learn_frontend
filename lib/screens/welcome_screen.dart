20309261bcb87e4417a0a34d65e2934dfed3a79b
Alan Chu <alanhdchu@utexas.edu>
2024-09-04 23:06:42 -0500

    fix ios issue

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  
+  Future<String> fetchMessage() async {
+  try {
+    print('fetchMessage123');
+    final response = await http.get(Uri.parse('http://192.168.1.72:8000/'));
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['message'];
+    } else {
+      throw Exception('Failed to load message: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error in fetchMessage: $e');
+    rethrow;
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,9 +34,22 @@ class WelcomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Welcome to Music Learn',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            FutureBuilder<String>(
+              future: fetchMessage(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();  // 加载时显示
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  return Text(
+                    snapshot.data!,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  );
+                } else {
+                  return Text('No message received');
+                }
+              },
             ),
             SizedBox(height: 50),
             Row(
